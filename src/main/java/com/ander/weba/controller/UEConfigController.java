3ca41c483d15e074ab8ae78ba19c38b6b5638ec8
zhaotz1 <zhaotz1@lenovo.com>
2018-11-24 16:54:51 +0800

    视频和图片 都 完整版本

--- src/main/java/com/ander/weba/controller/UEConfigController.java
+++ src/main/java/com/ander/weba/controller/UEConfigController.java
@@ -3,7 +3,8 @@ package com.ander.weba.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Value;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,13 +25,14 @@ import java.util.Map;
 @RestController
 @RequestMapping("/ue")
 public class UEConfigController {
-    @Value("D:\\apache-tomcat-9.0.13\\webapps\\fileSystem\\weba\\")
-    private String uploadPath;
+    private static Logger logger = LogManager.getLogger(UEConfigController.class);
+
 
     @RequestMapping("/conf")
     public Object conf(HttpServletRequest request, @RequestParam(required = false) String action) throws Exception {
         if (StringUtils.equals(action, "config")) {
-            String uirPre = "http://localhost:8080/fileSystem/weba/";
+            String uirPre = "http://" + request.getServerName() + ":" + request.getServerPort() + "/fileserver/files/weba/";
+
             ObjectMapper mapper = new ObjectMapper();
             File file = ResourceUtils.getFile("classpath:static/ue/jsp/config.json");
             ObjectNode objectNode = mapper.readValue(file, ObjectNode.class);
@@ -45,45 +47,35 @@ public class UEConfigController {
             return objectNode;
         } else if ("uploadimage".equals(action) || "uploadvideo".equals(action) || "uploadfile".equals(action)) {
             SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
-
+            String appPath = request.getSession().getServletContext().getRealPath(File.separator);
+            logger.info("获取的项目全路径是:" + appPath);
+            String uploadPath = appPath.substring(0, appPath.indexOf("webapps") + 7) + "/" + "fileserver/files/weba/";
             String date = sdf.format(new Date());
             String path = uploadPath + date;
-
-
             File temFile = new File(path);
-
             if (!temFile.exists()) {
                 temFile.mkdir();
             }
-
             MultipartHttpServletRequest mRequest = (MultipartHttpServletRequest) request;
-
             MultipartFile upfile = mRequest.getFile("upfile");
             if (upfile.isEmpty()) {
                 return "上传错误";
             }
-            String localFileName = "java" + System.currentTimeMillis() + upfile.getOriginalFilename();
-
+            String localFileName = "weba" + System.currentTimeMillis() + upfile.getOriginalFilename();
             try {
                 String filePath = path + "/" + localFileName;
                 upfile.transferTo(new File(filePath));
             } catch (Exception e) {
             }
-
             String imageUrl = date + "/" + localFileName;
-
             String prefix = upfile.getOriginalFilename().substring(upfile.getOriginalFilename().lastIndexOf(".") + 1);
-
             Map<String, Object> result = new HashMap();
             result.put("original", upfile.getOriginalFilename());
-
             result.put("name", upfile.getOriginalFilename());
             result.put("url", imageUrl);
             result.put("size", upfile.getSize());
             result.put("type", "." + prefix);
             result.put("state", "SUCCESS");
-
-
             return result;
         } else {
             return null;
